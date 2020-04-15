da8001c07ed0419dc08e90efed7e77762a622cbe
newilksliit <neranjana.wikum@my.sliit.lk>
2020-04-15 16:45:38 +0530

    IT16109254 Change Test

--- HealthcareSystem-BnT/src/com/Entity.java
+++ HealthcareSystem-BnT/src/com/Entity.java
@@ -1,12 +1,35 @@
 package com;
 
+import java.sql.Date;
+
 public class Entity {
-	public int id;
-	public String name;
+	private int id;
+	private String name;
+	private Date date;
+	
 	public Entity(int id, String name) {
 	//	super();
-		this.id = id;
+		this.setId(id);
+		this.setName(name);
+		this.setDate(new Date(System.currentTimeMillis()));
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
 		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getDate() {
+		return date;
+	}
+	void setDate(Date date) {
+		this.date = date;
 	}
 	
 }
