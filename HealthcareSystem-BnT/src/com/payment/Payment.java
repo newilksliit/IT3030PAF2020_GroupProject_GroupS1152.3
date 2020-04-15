4a456d7ec4129b3c422fb69445d4f0b2a443f36e
newilksliit <neranjana.wikum@my.sliit.lk>
2020-04-15 16:44:36 +0530

    IT16109254 Added attributes to the Entity Payment

--- HealthcareSystem-BnT/src/com/payment/Payment.java
+++ HealthcareSystem-BnT/src/com/payment/Payment.java
@@ -1,5 +1,41 @@
+////////////////////////		File History			//////////////////
+//------------------------------------------------------------------------
+//Date		User			Description
+//------	-----------		----------------------------------------------
+//150420	IT16109254		:Created
+//150420	IT16109254		:Added attributes
+//////////////////////////////////////////////////////////////////////////
 package com.payment;
+
+import java.sql.Date;
 
 public class Payment {
 
+	////////////region Private attributes/////
+	private 	int		refNumber	;
+	private 	Date 	date		;
+	private		float	amount		;
+	////////////endregion/////////////////////
+	
+	///////////	region	getters setters	///////
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getRefNumber() {
+		return refNumber;
+	}
+	public void setRefNumber(int refNumber) {
+		this.refNumber = refNumber;
+	}
+	/////////////endregion///////////////////////
+	
 }
