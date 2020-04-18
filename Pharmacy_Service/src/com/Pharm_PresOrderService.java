a1b7bfaf35c5df1134ae8b0d28a914fa53e59426
ShermilaFdo <shermilanishanifdo@gmail.com>
2020-04-18 14:27:11 +0530

    IT15089700 Add methods
    add two new methods to newly added class. Also add GET requests for
    those methods.

--- Pharmacy_Service/src/com/Pharm_PresOrderService.java
+++ Pharmacy_Service/src/com/Pharm_PresOrderService.java
@@ -23,17 +23,43 @@ import org.jsoup.nodes.Document;
 
 @Path("/Pharm_PresOrder")
 public class Pharm_PresOrderService {
-	
+
 	Pharm_PresOrder orderObj = new Pharm_PresOrder();
-	
+
 	//////
 	///////// Read Prescription Orders - Pharmacist
 	/////////////////////////////////////////////////////
 	@GET
-	@Path("/")
+	@Path("/Prescriptions")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String readPrescriptionOrders() {
 		return orderObj.readPrescriptionOrders();
+	}
+
+	//////
+	///////// Prescription Orders
+	///////// Read Only Available DRUGS - Pharmacist
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String readAvailableDrugs() {
+		return orderObj.readAvailableDrugs();
+	}
+
+	//////
+	///////// Without prescription Orders
+	//////// Read DRUGS by drug ID - Customer ////////////////
+	@GET
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String readDrugsByID(String drugData) {
+		JsonObject drugObject = new JsonParser().parse(drugData).getAsJsonObject();
+
+		Integer drugID = drugObject.get("drugID").getAsInt();
+
+		String output = orderObj.readDrugsByID(drugID);
+		return output;
 	}
 
 }
