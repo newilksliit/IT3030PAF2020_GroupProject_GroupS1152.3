72ddcdb32c38c2d64d3e0881e6307e2485780c77
newilksliit <neranjana.wikum@my.sliit.lk>
2020-04-19 21:19:27 +0530

    IT16109254 - Modified View

--- Payment_Services/src/com/View.java
+++ Payment_Services/src/com/View.java
@@ -1,10 +1,13 @@
 package com;
 
 import java.sql.Date;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -18,17 +21,28 @@ public class View {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response GetPayment() {
-		return Response.ok().entity(new PaymentController().readItems()).build();
+		return Response.ok().entity(PaymentController.readItems()).build();
 	}
-/*
+
 	@POST
-	@Path("/")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.TEXT_PLAIN })
+	public Response postMessage(List<Payment> list) {
+		System.out.println("Message received: " + list);
+		return Response.ok().entity(PaymentController.insertItem(list.get(0))).build();
+	}
+
+	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String insertItem() {
-		String output = itemObj.insertItem(itemCode, itemName, itemPrice, itemDesc);
-		return output;
+	public Response updateItem(List<Payment> list) {
+		return Response.ok().entity(PaymentController.updateItem(list.get(0))).build();
 	}
-	*/
 
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response deleteItem(List<Payment> list) {
+		return Response.ok().entity(PaymentController.deleteItem(list.get(0))).build();
+	}
 }
